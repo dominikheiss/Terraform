8e77714fc1341da1a3c179d037a6db935e5a3fb3
dominikheiss <70059992+dominikheiss@users.noreply.github.com>
2020-08-22 23:39:07 +0200

    new

--- Templates/basic4/variables.tf
+++ Templates/basic4/variables.tf
@@ -5,6 +5,5 @@ variable "prefix" {
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
-  type = "string"
   default = "West Europe"
 }
