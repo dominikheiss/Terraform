5f7a0fc2f479dc9d8d034226f1d994dff65b8fa6
dominikheiss <70059992+dominikheiss@users.noreply.github.com>
2020-08-22 21:36:11 +0200

    final-test

--- Templates/basic/main.tf
+++ Templates/basic/main.tf
@@ -9,35 +9,35 @@ resource "azurerm_resource_group" "rg_main" {
 
 resource "azurerm_virtual_network" "vnet_main" {
   name                = "${var.prefix}-vnet_main"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  location            = "${azurerm_resource_group.example.location}"
+  resource_group_name = "${azurerm_resource_group.name}"
+  location            = "${azurerm_resource_group.location}"
   address_space       = ["10.100.0.0/16"]
 }
 
 resource "azurerm_subnet" "Servers" {
   name                 = "Servers"
-  virtual_network_name = "${azurerm_virtual_network.example.name}"
-  resource_group_name  = "${azurerm_resource_group.example.name}"
+  virtual_network_name = "${azurerm_virtual_network.name}"
+  resource_group_name  = "${azurerm_resource_group.name}"
   address_prefixes     = ["10.100.1.0/24"]
 }
 
 resource "azurerm_subnet" "WVD" {
   name                 = "WVD"
-  virtual_network_name = "${azurerm_virtual_network.example.name}"
-  resource_group_name  = "${azurerm_resource_group.example.name}"
+  virtual_network_name = "${azurerm_virtual_network.name}"
+  resource_group_name  = "${azurerm_resource_group.name}"
   address_prefixes     = ["10.100.10.0/24"]
 }
 
 resource "azurerm_subnet" "BastionHost" {
   name                 = "BastionHost"
-  virtual_network_name = "${azurerm_virtual_network.example.name}"
-  resource_group_name  = "${azurerm_resource_group.example.name}"
+  virtual_network_name = "${azurerm_virtual_network.name}"
+  resource_group_name  = "${azurerm_resource_group.name}"
   address_prefixes     = ["10.100.254.0/24"]
 }
 
 resource "azurerm_subnet" "Gateway" {
   name                 = "Gateway"
-  virtual_network_name = "${azurerm_virtual_network.example.name}"
-  resource_group_name  = "${azurerm_resource_group.example.name}"
+  virtual_network_name = "${azurerm_virtual_network.name}"
+  resource_group_name  = "${azurerm_resource_group.name}"
   address_prefixes     = ["10.100.255.0/24"]
 }
