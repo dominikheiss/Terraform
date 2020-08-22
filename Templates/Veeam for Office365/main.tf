b8229bbecd3c72fee5427a388a2d6da04787a75c
dominikheiss <70059992+dominikheiss@users.noreply.github.com>
2020-08-23 01:04:08 +0200

    new

---
+++ Templates/Veeam for Office365/main.tf
@@ -0,0 +1,104 @@
+provider "azurerm" {
+  features {}
+}
+
+##### Create a resource group
+
+resource "azurerm_resource_group" "rg" {
+  name     				= "RG-${var.prefix}"
+  location 				= var.location
+}
+
+##### Create the VNET
+
+resource "azurerm_virtual_network" "vnet" {
+  name                	= "$VNET-{var.prefix}"
+  address_space 		= ["10.100.0.0/16"]
+  resource_group_name 	= azurerm_resource_group.rg.name
+  location 				= azurerm_resource_group.rg.location
+}
+
+##### Create a subnet for Azure Servers
+
+resource "azurerm_subnet" "Server" {
+  name 					= "Server" 
+  address_prefixes 		= ["10.100.1.0/24"]
+  virtual_network_name 	= azurerm_virtual_network.vnet.name
+  resource_group_name 	= azurerm_resource_group.rg.name
+}
+
+##### Create a subnet for Windows Virtual Desktops
+
+resource "azurerm_subnet" "WVD" {
+  name 					= "WVD" 
+  address_prefixes 		= ["10.100.10.0/24"]
+  virtual_network_name 	= azurerm_virtual_network.vnet.name
+  resource_group_name 	= azurerm_resource_group.rg.name
+}
+
+##### Create a subnet for Azure Bastion Host
+
+resource "azurerm_subnet" "AzureBastionSubnet" {
+  name 					= "AzureBastionSubnet" 
+  address_prefixes 		= ["10.100.254.0/24"]
+  virtual_network_name 	= azurerm_virtual_network.vnet.name
+  resource_group_name 	= azurerm_resource_group.rg.name
+}
+
+##### Create a subnet for VPN Gateway
+
+resource "azurerm_subnet" "Gateway" {
+  name 					= "Gateway" 
+  address_prefixes 		= ["10.100.255.0/24"]
+  virtual_network_name 	= azurerm_virtual_network.vnet.name
+  resource_group_name 	= azurerm_resource_group.rg.name
+}
+
+##### Create a Network Card
+
+resource "azurerm_network_interface" "nic-veeam" {
+  name                = "${var.prefix}-VeeamNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.Server.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "vm-veeam" {
+  name                  = "${var.prefix}-VeeamO365"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic-veeam.id]
+  vm_size               = "Standard_E2as_v4"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "veeam"
+    offer     = "office365backup"
+    sku       = "veeamoffice365backup"
+    version   = "4.0.2"
+  }
+  storage_os_disk {
+    name              = "veeamo365disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+  os_profile {
+    computer_name  = "VeeamO365"
+    admin_username = "Bechtle-adm"
+    admin_password = "Bechtle123!"
+  }
+  tags = {
+    environment = "staging"
+  }
+}
