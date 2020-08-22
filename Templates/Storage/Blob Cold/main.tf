d653d6d7b0156b5ff298160f625942e72d692020
dominikheiss <70059992+dominikheiss@users.noreply.github.com>
2020-08-23 00:17:32 +0200

    Update main.tf

--- Templates/Storage/Blob Cold/main.tf
+++ Templates/Storage/Blob Cold/main.tf
@@ -9,8 +9,8 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "storage" {
   name                     	= "${var.prefix}-storage1"
-  resource_group_name      	= azurerm_resource_group.storage.name
-  location                 	= azurerm_resource_group.storage.location
+  resource_group_name      	= azurerm_resource_group.rg.name
+  location                 	= azurerm_resource_group.rg.location
   account_tier             	= "Standard"
   account_replication_type 	= "LRS"
 }
