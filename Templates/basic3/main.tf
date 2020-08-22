b5e7287785ebe5d07605421f1b753ff61bfc8f4a
dominikheiss <70059992+dominikheiss@users.noreply.github.com>
2020-08-22 22:56:40 +0200

    Update main.tf

--- Templates/basic3/main.tf
+++ Templates/basic3/main.tf
@@ -3,31 +3,47 @@ provider "azurerm" {
 }
 
 # Create a resource group for core
-resource "azurerm_resource_group" "main-rg" {
-  name = "main-rg"
+resource "azurerm_resource_group" "rg_main" {
+  name = "rg_main"
   location = "west europe"
 }
 
-# Create the core VNET
-resource "azurerm_virtual_network" "main-vnet" {
-  name = "main-vnet"
+# Create the  VNET
+resource "azurerm_virtual_network" "vnet_main" {
+  name = "vnet_main"
   address_space = ["10.100.0.0/16"]
-  resource_group_name = azurerm_resource_group.main-rg.name
-  location = azurerm_resource_group.main-rg.location
+  resource_group_name = azurerm_resource_group.rg_main.name
+  location = azurerm_resource_group.rg_main.location
 }
 
-# Create a subnet for Azure Firewall
+# Create a subnet for Azure Servers
 resource "azurerm_subnet" "Server" {
-  name = "Server" # mandatory name -do not rename-
+  name = "Server" 
   address_prefixes = ["10.100.1.0/24"]
-  virtual_network_name = azurerm_virtual_network.main-vnet.name
-  resource_group_name = azurerm_resource_group.main-rg.name
+  virtual_network_name = azurerm_virtual_network.vnet_main.name
+  resource_group_name = azurerm_resource_group.rg_main.name
 }
 
-# Create a subnet for Azure Firewall
+# Create a subnet for Windows Virtual Desktops
 resource "azurerm_subnet" "WVD" {
-  name = "WVD" # mandatory name -do not rename-
+  name = "WVD" 
   address_prefixes = ["10.100.10.0/24"]
-  virtual_network_name = azurerm_virtual_network.main-vnet.name
-  resource_group_name = azurerm_resource_group.main-rg.name
+  virtual_network_name = azurerm_virtual_network.vnet_main.name
+  resource_group_name = azurerm_resource_group.rg_main.name
+}
+
+# Create a subnet for Azure Bastion
+resource "azurerm_subnet" "AzureBastionSubnet" {
+  name = "AzureBastionSubnet" 
+  address_prefixes = ["10.100.254.0/24"]
+  virtual_network_name = azurerm_virtual_network.vnet_main.name
+  resource_group_name = azurerm_resource_group.rg_main.name
+}
+
+# Create a subnet for Site 2 Site VPN
+resource "azurerm_subnet" "Gateway" {
+  name = "Gateway" 
+  address_prefixes = ["10.100.255.0/24"]
+  virtual_network_name = azurerm_virtual_network.vnet_main.name
+  resource_group_name = azurerm_resource_group.rg_main.name
 }
