a9f45ec2fc52759a40f19dd4a33ad3f691dd087a
dominikheiss <70059992+dominikheiss@users.noreply.github.com>
2020-08-22 23:40:37 +0200

    Update main.tf

--- Templates/basic4/main.tf
+++ Templates/basic4/main.tf
@@ -4,13 +4,13 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "$rg-{var.prefix}"
+  name     = "${var.prefix}-RG"
   location = "${var.location}"
 }
 
 # Create the  VNET
 resource "azurerm_virtual_network" "vnet" {
-  name = "$vnet-{var.prefix}"
+  name                = "${var.prefix}-VNET"
   address_space = ["10.100.0.0/16"]
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
