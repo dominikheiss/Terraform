e08f4603a76ec2ccc9c48ababa4b2d33070ade08
dominikheiss <70059992+dominikheiss@users.noreply.github.com>
2020-08-22 22:22:01 +0200

    Update main.tf

--- Templates/basic2/main.tf
+++ Templates/basic2/main.tf
@@ -9,8 +9,7 @@ provider "azurerm" {
 
 module "vnet" {
   source              = "Azure/vnet/azurerm"
-  resource_group_name = azurerm_resource_group.rg_rg-general-dhe.name
-  name                = "vnet_main"
+  resource_group_name = azurerm_resource_group.rg_rg-general-dhe.vnet_main
   address_space       = ["10.100.0.0/16"]
   subnet_prefixes     = ["10.100.1.0/24", "10.100.10.0/24", "10.100.254.0/24", "10.100.255.0/24"]
   subnet_names        = ["Server", "WVD", "AzureBastionSubnet", "Gateway"]
