63ba4fd66ea33d1cd80a8ca8c153a34d5356a397
dominikheiss <70059992+dominikheiss@users.noreply.github.com>
2020-08-23 00:24:11 +0200

    Update main.tf

--- Templates/Storage/Blob Cold/main.tf
+++ Templates/Storage/Blob Cold/main.tf
@@ -13,18 +13,11 @@ resource "azurerm_storage_account" "storage" {
   location                 	= azurerm_resource_group.rg.location
   account_tier             	= "Standard"
   account_replication_type 	= "LRS"
+  access_tier				= "Cool"
 }
 
 resource "azurerm_storage_container" "container" {
   name                  	= "data"
   storage_account_name  	= azurerm_storage_account.storage.name
   container_access_type 	= "private"
-}
-
-resource "azurerm_storage_blob" "blob" {
-  name                  	= "Veeam"
-  storage_account_name   	= azurerm_storage_account.storage.name
-  storage_container_name	= azurerm_storage_container.container.name
-  type                 		= "Block"
-  access_tier				= "Cool"
 }
