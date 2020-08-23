d47ff0b14f64751c315f17fd2f29fc6511fa638b
dominikheiss <70059992+dominikheiss@users.noreply.github.com>
2020-08-23 02:34:25 +0200

    Update main.tf

--- Templates/Veeam for Office365 + BastionHost/main.tf
+++ Templates/Veeam for Office365 + BastionHost/main.tf
@@ -122,8 +122,9 @@ resource "azurerm_virtual_machine" "vm-veeam" {
     admin_password = "Bechtle123!"
   }
   os_profile_windows_config {
-    enable_automatic_upgrades = false
-	provision_vm_agent = true
+    enable_automatic_upgrades 	= false
+	provision_vm_agent 			= true
+	timezone					= "W. Europe Standard Time"
   }
   tags = {
     environment = "staging"
@@ -153,3 +154,31 @@ resource "azurerm_bastion_host" "bastionhost" {
     public_ip_address_id = azurerm_public_ip.bastion-ip.id
   }
 }
+
+##### Create Veeam APP for Modern Authentication
+
+resource "azuread_application" "veeamapp" {
+  name = "VeeamAPP"
+  required_resource_access {
+    # MicrosoftGraph API
+    resource_app_id = "00000003-0000-0000-c000-000000000000"
+
+    # APPLICATION PERMISSIONS: "Read directory data":
+    # 7ab1d382-f21e-4acd-a863-ba3e13f7da61
+    resource_access {
+      id   = "7ab1d382-f21e-4acd-a863-ba3e13f7da61"
+      type = "Role"
+    }
+    # APPLICATION PERMISSIONS: "Read group data":
+    # 5b567255-7703-4780-807c-7be8301ae99b
+    resource_access {
+      id   = "5b567255-7703-4780-807c-7be8301ae99b"
+      type = "Role"
+    }	
+}
+
+output "azure_ad_object_id" {
+  value = data.azuread_application.veeamapp.id
+}
+
+
