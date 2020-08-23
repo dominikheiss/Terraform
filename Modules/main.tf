e44033fcc7f7cd735ddd9711e9b7b7d49df1afa7
dominikheiss <70059992+dominikheiss@users.noreply.github.com>
2020-08-23 19:20:10 +0200

    Update main.tf

--- Modules/main.tf
+++ Modules/main.tf
@@ -8,14 +8,15 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "network" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = "10.100.0.0/16"
-  subnet_prefixes     = ["10.1000.1.0/24", "10.100.10.0/24", "10.100.254.0/24", "10.100.255.0/24"]
-  subnet_names        = ["Server", "WVD", "AzureBastionSubnet", "Gateway"]
+  source              						= "Azure/network/azurerm"
+  resource_group_name 						= azurerm_resource_group.rg.name
+  vnet_name									= "TEST-VNET1"
+  address_space       						= "10.100.0.0/16"
+  subnet_prefixes     						= ["10.100.1.0/24", "10.100.10.0/24", "10.100.254.0/24", "10.100.255.0/24"]
+  subnet_names        						= ["Server", "WVD", "AzureBastionSubnet", "Gateway"]
 
   tags = {
-    environment = "IT"
-    application = "Network"
+    environment 							= "IT"
+    application 							= "Network"
   } 
 }
