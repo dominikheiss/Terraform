b748ef1ce49306da8bef66e4b8cf882d7c9d8d9c
dominikheiss <70059992+dominikheiss@users.noreply.github.com>
2020-08-24 15:29:41 +0200

    Update main.tf

--- Templates/Modules/1. Ressource Group + Vnet + Subnets/Basic (with variables)/main.tf
+++ Templates/Modules/1. Ressource Group + Vnet + Subnets/Basic (with variables)/main.tf
@@ -3,20 +3,20 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     									= "${var.prefix}-RG1"
-  location 									= var.location
+  name     							      		= "${var.prefix}-RG1"
+  location 								        = var.location
 }
 
 module "network" {
   source              						= "Azure/network/azurerm"
   resource_group_name 						= azurerm_resource_group.rg.name
-  vnet_name									= "${var.prefix}-VNET1"
+  vnet_name									      = "${var.prefix}-VNET1"
   address_space       						= "10.100.0.0/16"
   subnet_prefixes     						= ["10.100.1.0/24", "10.100.10.0/24", "10.100.254.0/24", "10.100.255.0/24"]
   subnet_names        						= ["Server", "WVD", "AzureBastionSubnet", "Gateway"]
 
   tags = {
-    environment 							= "IT"
-    application 							= "Network"
+    environment 						    	= "IT"
+    application 						    	= "Network"
   } 
 }
