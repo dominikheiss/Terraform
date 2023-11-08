aadb97fda8545f206db0d3ef86ea435f1891f908
dominikheiss <70059992+dominikheiss@users.noreply.github.com>
2023-11-08 11:52:00 +0100

    Update main.tf

--- Customers/Dehner/VNET_Subnet_Peering_Tags - Chatgpt/main.tf
+++ Customers/Dehner/VNET_Subnet_Peering_Tags - Chatgpt/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "spoke" {
 }
 
 resource "azurerm_virtual_network" "spoke_vnet" {
-  name                = var.vnet_name
+  name                = vnet-${var.vnet_name}
   address_space       = var.address_space
   location            = var.location
   resource_group_name = azurerm_resource_group.spoke.name
@@ -13,7 +13,7 @@ resource "azurerm_virtual_network" "spoke_vnet" {
 }
 
 resource "azurerm_subnet" "spoke_subnet" {
-  name                 = "${var.vnet_name}-subnet"
+  name                 = "snet-${var.vnet_name}"
   resource_group_name  = azurerm_resource_group.spoke.name
   virtual_network_name = azurerm_virtual_network.spoke_vnet.name
   address_prefixes     = var.address_space
