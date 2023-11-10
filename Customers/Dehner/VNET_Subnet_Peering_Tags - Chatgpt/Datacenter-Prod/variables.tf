28bcbcea8de789abea07a2b8ab5ed09dcc2c6fe6
dominikheiss <70059992+dominikheiss@users.noreply.github.com>
2023-11-10 11:16:05 +0100

    final

--- Customers/Dehner/VNET_Subnet_Peering_Tags - Chatgpt/Datacenter-Prod/variables.tf
+++ Customers/Dehner/VNET_Subnet_Peering_Tags - Chatgpt/Datacenter-Prod/variables.tf
@@ -1,16 +1,11 @@
 variable "subscription_id" {
   description = "Definition of proper Subscription ID"
-  default     = "f96bb086-c28f-40a3-88a8-89de63160a72"
-}
-
-variable "hub_subscription_id" {
-  description = "Definition of HUB Subscription ID"
-  default     = "e635aa2a-2f4a-432e-8d5a-07f9ddce0808"
+  default     = "f96bb086-c28f-40a3-88a8-89de63160a72"    // Bitte je nach Subscription ändern
 }
 
 variable "vnet_name" {
   description = "Name of the VNet"
-  default = "datacenter-prod"
+  default = "datacenter-prod"   // Bitte je nach Subscription ändern
 }
 
 variable "address_space" {
@@ -27,7 +22,8 @@ variable "subnets" {
   }))
   default = {
     "snet" = {
-      address_prefixes = ["10.180.18.0/24"]
+      address_prefixes = ["10.180.18.0/24"]   // Bitte je nach Subscription ändern
+
       // Setzen Sie weitere Standardwerte für zusätzliche Eigenschaften hier
     }
     // Fügen Sie weitere Subnetze nach Bedarf hinzu
@@ -63,7 +59,7 @@ variable "asset_tags" {
 
 
 
-// HARD CODED VARIABLES - DO NOT CHANGE
+// -----> HARD CODED VARIABLES - DO NOT CHANGE <-----
 
 variable "location" {
   description = "The Azure Region where all resources in this configuration should be created."
@@ -73,6 +69,11 @@ variable "location" {
 variable "resource_group_name" {
   description = "Name of the resource group"
   default = "network"
+}
+
+variable "hub_subscription_id" {
+  description = "Definition of HUB Subscription ID"
+  default     = "e635aa2a-2f4a-432e-8d5a-07f9ddce0808"  // Hardcoded - does not change!
 }
 
 variable "hub_resource_group_name" {
